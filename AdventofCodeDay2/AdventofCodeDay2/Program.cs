7abc5691c218d09cef52bbd2aa4fbed750aa6973
yangleon8 <yangleon8@gmail.com>
2022-12-08 09:48:33 +1100

    add days 1 and 2

---
+++ AdventofCodeDay2/AdventofCodeDay2/Program.cs
@@ -0,0 +1,50 @@
+﻿int total = 0;
+foreach (string line in System.IO.File.ReadLines(@"c:\Users\user\Documents\input2.txt"))
+{
+    Console.WriteLine(line);
+    //X :lose Y:draw Z: win
+    //A B C
+    string[] letters = line.Split(' ');
+   
+
+    if (letters[0] == "A" && letters[1] == "X")
+    {
+        total = total + 3;
+    }
+    else if (letters[0] == "A" && letters[1] == "Z")
+    {
+        total = total + 6 + 2;
+    }
+    else if (letters[0] == "A" && letters[1] == "Y")
+    {
+        total = total + 3 + 1;
+    }
+
+    if (letters[0] == "B" && letters[1] == "X")
+    {
+        total = total + 1;
+    }
+    else if (letters[0] == "B" && letters[1] == "Z")
+    {
+        total = total + 6 + 3;
+    }
+    else if (letters[0] == "B" && letters[1] == "Y")
+    {
+        total = total + 3 + 2;
+    }
+
+    if (letters[0] == "C" && letters[1] == "X")
+    {
+        total = total + 2;
+    }
+    else if (letters[0] == "C" && letters[1] == "Z")
+    {
+        total = total + 6 + 1;
+    }
+    else if (letters[0] == "C" && letters[1] == "Y")
+    {
+        total = total + 3 + 3;
+    }
+
+}
+Console.WriteLine(total);
---
+++ adventofCodeDay 1/adventofCodeDay 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.Metrics;
+string text = System.IO.File.ReadAllText(@"C:\input.txt");
+List<int> calList = new List<int>();
+int cumVal = 0;
+foreach (string line in System.IO.File.ReadLines(@"c:\input.txt"))
+{
+    if(line != "")
+    {
+        cumVal = cumVal +Int32.Parse(line);
+        
+    }
+    else
+    {
+        calList.Add(cumVal);
+        cumVal = 0;
+    }
+
+}
+calList.Sort();
+var t = calList[calList.Count - 1] + calList[calList.Count - 2] + calList[calList.Count - 3];
+Console.WriteLine(t);
